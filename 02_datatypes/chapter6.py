51c0b303f23cb85c9de8e46387b19eb5e540a49e
Rupam-Mondal <rupammondal564@gmail.com>
2026-01-15 11:48:43 +0530

    Tuples basic

---
+++ 02_datatypes/chapter6.py
@@ -0,0 +1,11 @@
+# String
+
+chai_type = "ginger"
+customer_name = "priya"
+
+
+# immutable
+
+print(chai_type[0:3]) # 3 is exclusive
+
+print(chai_type[::-1]) # rotate the string
---
+++ 02_datatypes/chapter7.py
@@ -0,0 +1,11 @@
+# Tuples
+
+spices = (1 , 2 , "massala")
+
+(one , two , three) = spices
+print(one)
+print(spices)
+
+val1 , val2 , val3 = 1 , 2 , 3 # behind the scene we are using tuple here
+
+print(f"Is this present in spices = {1 in spices}")
